96adb2a60687de9caf29a73dcd60fb7a04e51df3
A-Kabbara <112920055+A-Kabbara@users.noreply.github.com>
2022-09-09 20:14:09 +1000

    Add files via upload

---
+++ Week1/todo.js
@@ -0,0 +1,10 @@
+let vue = new Vue({
+    el: '#app',
+    data: {
+        todos: [
+            { title: 'assignemnt1', checked: true},
+            { title: 'assignemnt2', checked: false},
+            { title: 'assignemnt3', checked: false},
+        ],
+    },
+});
